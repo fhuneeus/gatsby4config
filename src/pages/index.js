4974b5fe3d8c2c24afa115d6c30f4560ff83dd6a
Federico Huneeus <fahuneeu@uc.cl>
2022-03-04 10:12:06 -0300

    Changing title of index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,12 +131,13 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations! Welcome to the gatsby4 config build.
+        Congratulations! Welcome to the gatsby4 config build. Pushing new content.
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
           🎉🎉🎉
         </span>
+        Publishing new content for deploys.
       </h1>
       <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
